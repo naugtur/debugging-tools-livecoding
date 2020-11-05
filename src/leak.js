654bc4496faaf2b92559d7974083157decdf2d48
naugtur <naugtur@gmail.com>
2020-11-05 12:45:12 +0100

    check heap after GC

--- src/leak.js
+++ src/leak.js
@@ -1,3 +1,10 @@
+"use strict";
 const v8 = require("v8");
 
-console.log(v8.getHeapSpaceStatistics());
+const { PerformanceObserver } = require("perf_hooks");
+
+const obs = new PerformanceObserver((list) => {
+  console.log(list.getEntries());
+  console.log(v8.getHeapSpaceStatistics());
+});
+obs.observe({ entryTypes: ["gc"], buffered: false });
